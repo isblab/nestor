Fixed H computation

utils/evidence_plot.py
@@ -1,32 +1,46 @@
 import os,sys
 import math
+import glob
 import numpy as np
 from matplotlib import pyplot as plt
 
-trial_id = sys.argv[1]
-files = ['res_01/estimated_evidences.dat',\
-        'res_05/estimated_evidences.dat',\
-        'res_10/estimated_evidences.dat',\
-        'res_20/estimated_evidences.dat',\
-        'res_30/estimated_evidences.dat',\
-        'res_50/estimated_evidences.dat']
-
-mean_log_evi = []
-for res in files:
+def get_evidences_H(resolution_dir):
+    run_dirs = glob.glob(os.path.join(resolution_dir,'run*'))
     evidences = []
-    log_evidences = []
+    analytical_uncertainties = []
+    for run in run_dirs:
+        run_log_file = os.path.join(run,'run.log')
+        try:
+            with open(run_log_file,'r') as rlf:
+                for ln in rlf.readlines():
+                    if ln.startswith('Accumulated evidence'):
+                        evidences.append(math.log(float(ln.strip().split(': ')[-1])))
+                    if ln.startswith('Analytical uncertainty'):
+                        analytical_uncertainties.append(float(ln.strip().split(': ')[-1]))
+        except FileNotFoundError:
+            print('Shuffle configuration error found. Skipping that run...')
 
-    with open(res,'r') as evf:
-        for ln in evf.readlines():
-            evidence = float(ln.strip())
-            evidences.append(evidence)
-            log_evidences.append(-math.log(evidence))
+    return evidences, analytical_uncertainties
 
-    std_err = np.std(log_evidences)/math.sqrt(len(log_evidences))
-    mean_log_evi.append(np.mean(log_evidences))
-    plt.errorbar(res.split('/')[0], np.mean(log_evidences), yerr=std_err, fmt='o')
 
-plt.yticks(np.arange(int(min(mean_log_evi)-std_err),int(max(mean_log_evi))+std_err,2))
-plt.grid(axis='y')
-# plt.savefig(f'trial_{trial_id}_evidence.png') # Include trial number in fname
+resolutions = sys.argv[1:]
+
+x_vals = []
+evi_mean_vals = []
+evi_std_err_vals = []
+for res in resolutions:
+    try:
+        evidences, _ = get_evidences_H('res_'+res+'/')
+        evi_std_err = np.std(evidences)/math.sqrt(len(evidences))
+        evi_mean = np.mean(evidences)
+
+        x_vals.append(f'res_{res}')
+        evi_mean_vals.append(evi_mean)
+        evi_std_err_vals.append(evi_std_err)
+        plt.errorbar(f'res_{res}', evi_mean, yerr=evi_std_err, marker='o')
+
+
+    except FileNotFoundError:
+        print('Shuffle configuration error found. Skipping that run...')
+
 plt.show()

utils/get_uncertainty_plots.py
@@ -2,6 +2,7 @@ import os
 import sys
 import glob
 import math
+import yaml
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,11 +16,12 @@ def get_evidences_H(resolution_dir):
             with open(run_log_file,'r') as rlf:
                 for ln in rlf.readlines():
                     if ln.startswith('Accumulated evidence'):
-                        evidences.append(float(ln.strip().split(': ')[-1]))
+                        evidences.append(math.log(float(ln.strip().split(': ')[-1])))
                     if ln.startswith('Analytical uncertainty'):
                         analytical_uncertainties.append(float(ln.strip().split(': ')[-1]))
         except FileNotFoundError:
-            print('Shuffle configuration error found. Skipping that run...')
+            pass
+            # print('Some error occurred in the run. Skipping that run...')
 
     return evidences, analytical_uncertainties
 
@@ -28,26 +30,48 @@ resolutions = sys.argv[1:]
 
 x_vals = []
 ana_unc_vals = []
-evi_std_vals = []
+ana_unc_mean_vals = []
+evidence_mean_vals = []
+evi_std_err_vals = []
+
 for res in resolutions:
-    try:
-        evidences, ana_unc = get_evidences_H('res_'+res+'/')
-        evidence_std = np.std(evidences)
-        ana_unc_mean = np.mean(ana_unc)
+    evidences, ana_unc = get_evidences_H('res_'+res+'/')
+    evidence_std_err = np.std(evidences)/math.sqrt(len(evidences))
 
+    evidence_mean_vals.append(np.mean(evidences))
+
+    for unc in ana_unc:
         x_vals.append(f'res_{res}')
-        ana_unc_vals.append(ana_unc_mean)
-        evi_std_vals.append(math.log(evidence_std))
-    except FileNotFoundError:
-        print('Shuffle configuration error found. Skipping that run...')
+        ana_unc_vals.append(unc)
 
-fig,ax1 = plt.subplots()
-ax1.plot(x_vals,ana_unc_vals, marker='o', c='C1',label='Analytical uncertainties')
-ax1.set_xlabel('Resolutions')
-ax1.set_ylabel('Analytical uncertainties')
+    ana_unc_mean_vals.append(np.mean(ana_unc))
+    evi_std_err_vals.append(evidence_std_err)
+    print(f"Analytical uncertainty standard deviation: {np.std(ana_unc)}") #/math.sqrt(len(ana_unc))}
 
-ax2 = ax1.twinx()
-ax2.plot(x_vals,evi_std_vals,marker='o', c='C2',label='Log(std(evidences))')
-ax2.set_ylabel('Log standard deviations of evidences')
+
+uniq_x = []
+for x in x_vals:
+    if x not in uniq_x:
+        uniq_x.append(x)
+
+
+fig,ax = plt.subplots(3, sharex=True)
+
+ax[0].errorbar(uniq_x, evidence_mean_vals, yerr=ana_unc_mean_vals, marker='o', label='logZ with ana_unc errorbar')
+ax[0].set_ylabel('Log evidence')
+
+ax[1].errorbar(uniq_x, evidence_mean_vals, yerr=evi_std_err_vals, marker='o', c='C3', label='logZ with std error errorbar')
+ax[1].set_ylabel('Log evidence')
+
+ax[2].scatter(x_vals,ana_unc_vals, c='C2', marker='o', label='Analytical uncertainties')
+ax[2].plot(uniq_x, ana_unc_mean_vals, marker='*', c='C2', label='Mean analytical uncertainties')
+ax[2].set_xlabel('Resolutions')
+ax[2].set_ylabel('Analytical uncertainties')
+
+ax2 = ax[2].twinx()
+ax2.plot(uniq_x, evi_std_err_vals, c='C4', marker='o', label='Log(std(evidences))')
+ax2.set_ylabel('Std. deviations of log(evidences)')
+
 fig.legend()
-fig.savefig('tmp.png')
+# fig.savefig('uncertainty_plot.png')
+plt.show()