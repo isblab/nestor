ee32d8d50fb380a603384c0d2704bae97de6bb62
Ben Webb <ben@salilab.org>
2024-05-22 15:17:26 -0700

    Remove redundant section

--- examples/nude_modeling.py
+++ examples/nude_modeling.py
@@ -31,15 +31,6 @@ import sys
 
 IMP.setup_from_argv(sys.argv, "Application of NestOR to the NuDe subcomplex")
 
-
-# --- remove this section
-# print("This example does not work out of the box. Please read the README "
-#       "file to obtain necessary inputs and then remove this section "
-#       "of the script.")
-# sys.exit(0)
-# --- remove this section
-
-
 dat_dir = IMP.nestor.get_example_path("input")  
 run_output_dir = "run_" + "0"  # sys.argv[1]
 topology_file = os.path.join(dat_dir, "topology50.txt")  # sys.argv[2]
